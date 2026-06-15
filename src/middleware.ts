2e418c3fd4dc1f3913bddb72cf8853cc88efa712
Dr. Samuel E. Wilson <deploy@samuelewilson.org>
2026-06-15 05:42:33 +0000

    Update middleware matcher to match existing site config

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,5 +16,5 @@ export default clerkMiddleware(async (auth, req) => {
 });
 
 export const config = {
-  matcher: ["/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)","/(api|trpc)(.*)"],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
